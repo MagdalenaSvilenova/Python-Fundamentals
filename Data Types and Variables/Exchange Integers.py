56d45339f2182601356ca937c29cd214c1623eeb
MagdalenaSvilenova <m_svilenova@abv.bg>
2021-01-04 17:38:53 +0200

    Added resource

---
+++ Data Types and Variables/Exchange Integers.py
@@ -0,0 +1,12 @@
+first_num = int(input())
+second_num = int(input())
+print('Before:')
+print(f'a = {first_num}')
+print(f'b = {second_num}')
+c = first_num
+second_num = first_num
+c = second_num
+
+print('After:')
+print(f'a = {first_num}')
+print(f'b = {second_num}')
