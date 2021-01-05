65638b8bda0c527d6d4d8d1596648bae2d51e905
MagdalenaSvilenova <m_svilenova@abv.bg>
2021-01-05 18:46:49 +0200

    Added resource

---
+++ Data Types and Variables/Prime Number Checker.py
@@ -0,0 +1,9 @@
+import math
+num_one = int(input())
+num_second = int(input())
+num_third = int(input())
+num_fourth = int(input())
+
+result = (math.floor((num_one + num_second) / num_third)) * num_fourth
+
+print(result)
