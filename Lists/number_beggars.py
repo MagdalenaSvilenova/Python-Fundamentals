ad71542fb2b04ffc1e1845a0e59898fd7049015e
MagdalenaSvilenova <m_svilenova@abv.bg>
2021-01-14 16:21:19 +0200

    Add files via upload

---
+++ Lists/number_beggars.py
@@ -0,0 +1,20 @@
+nums_str = input().split(', ')
+beggars_count = int(input())
+numbers = []
+
+for nums_str in nums_str:
+    number = int(nums_str)
+    numbers.append(number)
+
+beggars = []
+for i in range(beggars_count):
+    beggars.append(0)
+
+index = 0
+for number in numbers:
+    beggars[index] += number
+    index += 1
+    if index == beggars_count:
+        index = 0
+
+print(beggars)
